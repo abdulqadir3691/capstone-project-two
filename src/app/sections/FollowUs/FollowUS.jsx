817be2c7e13886d7edef8a1a74816d8a9ad2fa11
Abdul Qadir <107254626+abdulqadir3691@users.noreply.github.com>
2023-09-15 19:40:21 +0500

    pisc path updated

--- src/app/sections/FollowUs/FollowUS.jsx
+++ src/app/sections/FollowUs/FollowUS.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./FollowUs.css";
 import Image from "next/image";
-import Instagram1 from "../../../assets/Instagram Post.svg";
-import Instagram2 from "../../../assets/Instagram Post 2.svg";
-import Instagram3 from "../../../assets/Instagram Post 3.svg";
-import Instagram4 from "../../../assets/Instagram Post 4.svg";
-import Instagram5 from "../../../assets/Instagram Post 5.svg";
-import Instagram6 from "../../../assets/Instagram Post 6.svg";
+import Instagram1 from "../../../assets/InstagramPost.svg"
+import Instagram2 from "../../../assets/InstagramPost2.svg";
+import Instagram3 from "../../../assets/InstagramPost3.svg";
+import Instagram4 from "../../../assets/InstagramPost4.svg";
+import Instagram5 from "../../../assets/InstagramPost5.svg";
+import Instagram6 from "../../../assets/InstagramPost6.svg";
 
 const TopCatagoryProduct = () => {
   return (
